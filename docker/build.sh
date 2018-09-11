614d44ab88bf709892f80946c9ae80ad137a81cb
Andrej Babic <andrej.babic@psi.ch>
2018-09-11 14:17:07 +0200

    Adjust docker builder

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VERSION=1.2.0
-docker build --no-cache=true -t docker.psi.ch:5000/psen_processing .
-docker tag docker.psi.ch:5000/psen_processing docker.psi.ch:5000/psen_processing:$VERSION
-docker push docker.psi.ch:5000/psen_processing:$VERSION
-docker push docker.psi.ch:5000/psen_processing
+VERSION=0.0.1
+docker build --no-cache=true -t docker.psi.ch:5000/psss_processing .
+docker tag docker.psi.ch:5000/psss_processing docker.psi.ch:5000/psss_processing:$VERSION
+docker push docker.psi.ch:5000/psss_processing:$VERSION
+docker push docker.psi.ch:5000/psss_processing

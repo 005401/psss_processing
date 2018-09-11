2e2a640d2e23c14d658717f3f4752a1b440e4a97
Andrej Babic <andrej.babic@psi.ch>
2018-09-11 17:56:19 +0200

    Test classic interaction

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,8 +6,8 @@ from bsread.sender import sender
 from bsread import source, PULL, json
 from multiprocessing import Process
 
-from psen_processing import config, PsenProcessingClient
-from psen_processing.start_processing import start_processing
+from psss_processing import PsssProcessingClient, config
+from psss_processing.start_processing import start_processing
 
 
 class TestClient(unittest.TestCase):
@@ -22,16 +22,18 @@ class TestClient(unittest.TestCase):
         data_to_send = {self.pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE: self.image}
 
         def send_data():
-            with sender(port=11000) as output_stream:
+            with sender(port=10001) as output_stream:
+                sleep(1)
                 for x in range(self.n_images):
                     output_stream.send(data=data_to_send)
 
         def process_data():
-            start_processing(input_stream="tcp://localhost:11000",
+            start_processing(input_stream="tcp://localhost:10001",
                              output_stream_port=12000,
                              rest_api_interface="0.0.0.0",
                              rest_api_port=10000,
                              epics_pv_name_prefix=self.pv_name_prefix,
+                             output_pv="JUST_SOMETHING",
                              auto_start=False)
 
         self.sending_process = Process(target=send_data)
@@ -39,7 +41,6 @@ class TestClient(unittest.TestCase):
 
         self.sending_process.start()
         self.processing_process.start()
-
         sleep(1)
 
     def tearDown(self):
@@ -52,31 +53,35 @@ class TestClient(unittest.TestCase):
         sleep(1)
 
     def test_classic_interaction(self):
-        client = PsenProcessingClient("http://localhost:10000/")
+        client = PsssProcessingClient("http://localhost:10000/")
 
         self.assertEqual(client.get_status(), "stopped")
 
-        self.assertListEqual(client.get_roi_signal(), [])
-        self.assertListEqual(client.get_roi_background(), [])
+        self.assertListEqual(client.get_roi(), [])
+        self.assertDictEqual(client.get_parameters(), config.DEFAULT_PARAMETERS)
 
-        client.set_roi_signal(None)
-        self.assertListEqual(client.get_roi_signal(), [])
+        client.set_roi(None)
+        self.assertListEqual(client.get_roi(), [])
 
         with self.assertRaisesRegex(ValueError, "ROI offsets"):
-            client.set_roi_signal([-1, -1, -1, -1])
+            client.set_roi([-1, -1, -1, -1])
 
-        roi_signal = [0, 1024, 0, 1024]
-        client.set_roi_signal(roi_signal)
-        self.assertListEqual(client.get_roi_signal(), roi_signal)
+        roi = [0, 1024, 0, 1024]
+        client.set_roi(roi)
+        self.assertListEqual(client.get_roi(), roi)
 
-        roi_background = [0, 100, 0, 100]
-        client.set_roi_background(roi_background)
-        self.assertListEqual(client.get_roi_background(), roi_background)
+        parameters = {"threshold": 10,
+                      "rotation": 45}
+        client.set_parameters(parameters)
+        self.assertDictEqual(client.get_parameters(), parameters)
 
         self.assertDictEqual(client.get_statistics(), {})
 
         client.start()
         self.assertEqual(client.get_status(), "processing")
+
+        # Wait for PV connection timeout.
+        sleep(1)
 
         statistics = client.get_statistics()
         self.assertEqual(len(statistics), 4)
@@ -87,18 +92,20 @@ class TestClient(unittest.TestCase):
 
         processed_data = []
 
-        with source(host="localhost", port=12000, mode=PULL, receive_timeout=1000) as input_stream:
+        with source(host="localhost", port=12000, mode=PULL) as input_stream:
             for index in range(self.n_images):
                 processed_data.append(input_stream.receive())
 
         statistics = client.get_statistics()
-        self.assertEqual(client.get_statistics()["n_processed_images"], self.n_images)
+
+        self.assertEqual(statistics["n_processed_images"], self.n_images)
 
         # Pulse ids are 0 based.
-        self.assertEqual(client.get_statistics()["last_sent_pulse_id"], self.n_images - 1)
+        self.assertEqual(statistics["last_sent_pulse_id"], self.n_images - 1)
 
         self.assertTrue("processing_start_time" in statistics)
         self.assertTrue("last_sent_time" in statistics)
+        self.assertTrue("image" not in statistics)
 
         self.assertEqual(client.get_status(), "processing")
 

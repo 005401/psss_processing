c1a1fd53e5acc0936b285ce99742db70e9c936b2
Andrej Babic <andrej.babic@psi.ch>
2018-09-20 16:26:42 +0200

    Fix processor tests

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -16,17 +16,17 @@ class TestProcessing(unittest.TestCase):
         image = numpy.zeros(shape=(1024, 512), dtype="uint16")
         image += 1
 
-        self.assertEqual(manipulate_image(image, None, 0, 0).shape, (1024, 512))
-        self.assertEqual(manipulate_image(image, None, 0, 90).shape, (512, 1024))
+        self.assertEqual(manipulate_image(image, None, 0, 0, 0).shape, (1024, 512))
+        self.assertEqual(manipulate_image(image, None, 0, 0, 90).shape, (512, 1024))
 
-        new_shape = manipulate_image(image, None, 0, 45).shape
+        new_shape = manipulate_image(image, None, 0, 0, 45).shape
         # Rotating by 45 degree should always give you a square.
         self.assertEqual(new_shape[0], new_shape[1])
         # The threshold should remove all data.
-        self.assertEqual(manipulate_image(image, None, 2, 0).sum(), 0)
+        self.assertEqual(manipulate_image(image, None, 2, 0, 0).sum(), 0)
 
         roi = [200, 300, 200, 150]
-        self.assertEqual(manipulate_image(image, roi, 0, 45).shape, (150, 300))
+        self.assertEqual(manipulate_image(image, roi, 0, 0, 45).shape, (150, 300))
 
     def test_process_image(self):
         image = numpy.zeros(shape=(1024, 512), dtype="uint16")
@@ -36,9 +36,10 @@ class TestProcessing(unittest.TestCase):
 
         roi = [0, 512, 0, 1024]
 
-        image, processed_data = process_image(image, image_property_name, roi, 15, 90)
+        image, processed_data = process_image(image, image_property_name, roi, 15, 30, 90)
         self.assertSetEqual(set(processed_data.keys()), {image_property_name + ".processing_parameters",
-                                                         image_property_name + ".spectrum"})
+                                                         image_property_name + ".spectrum",
+                                                         image_property_name})
 
         # After rotating for 90 degrees this is the largest image we can get for the ROI.
         self.assertEqual(image.shape, (512, 512))
@@ -47,7 +48,8 @@ class TestProcessing(unittest.TestCase):
 
         processing_parameters = json.loads(processed_data[image_property_name + ".processing_parameters"])
 
-        self.assertEqual(processing_parameters["threshold"], 15)
+        self.assertEqual(processing_parameters["min_threshold"], 15)
+        self.assertEqual(processing_parameters["max_threshold"], 30)
         self.assertEqual(processing_parameters["rotation"], 90)
         self.assertListEqual(processing_parameters["roi"], roi)
 
@@ -55,7 +57,8 @@ class TestProcessing(unittest.TestCase):
         pv_name_prefix = "JUST_TESTING"
         n_images = 5
         original_roi = [100, 300, 100, 200]
-        original_parameters = {"threshold": 0,
+        original_parameters = {"min_threshold": 0,
+                               "max_threshold": 0,
                                "rotation": 90}
 
         image = numpy.zeros(shape=(1024, 512), dtype="uint16")
@@ -105,8 +108,9 @@ class TestProcessing(unittest.TestCase):
         processing_parameters = json.loads(parameters)
 
         self.assertEqual(processing_parameters["roi"], original_roi)
-        self.assertEqual({"threshold": processing_parameters["threshold"],
-                         "rotation": processing_parameters["rotation"]}, original_parameters)
+        self.assertEqual({"min_threshold": processing_parameters["min_threshold"],
+                         "rotation": processing_parameters["rotation"],
+                          "max_threshold": processing_parameters["max_threshold"]}, original_parameters)
 
         spectrum = final_data[0].data.data[pv_name_prefix + config.EPICS_PV_SUFFIX_IMAGE + ".spectrum"].value
 

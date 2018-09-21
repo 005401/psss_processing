bff7aae3f05c5e16942eb2cee73fb9f127fb26ef
Andrej Babic <andrej.babic@psi.ch>
2018-09-21 15:48:15 +0200

    Add performance tests

---
+++ tests/perf_test.py
@@ -0,0 +1,49 @@
+import unittest
+from time import time
+
+import numpy
+
+from psss_processing.processor import process_image, manipulate_image
+
+
+class ImageProcessingPerformance(unittest.TestCase):
+
+    def test_processor_performance(self):
+        # Profile only if LineProfiler present.
+        # To install: conda install line_profiler
+        try:
+            from line_profiler import LineProfiler
+        except ImportError:
+            print("Please install the 'line_profiler' module first.")
+            return
+
+        image = (numpy.random.rand(2048, 2048) * 100).astype(dtype="uint16")
+
+        roi = [100, 1848, 0, 2048]
+        min_threshold = 5
+        max_threshold = 70
+        rotation = 0
+
+        profile = LineProfiler(process_image)
+        process_image_wrapper = profile(process_image)
+        profile.add_function(manipulate_image)
+
+        n_iterations = 500
+
+        start_time = time()
+
+        for _ in range(n_iterations):
+            process_image_wrapper(image, "image", roi, min_threshold, max_threshold, rotation)
+
+        end_time = time()
+
+        time_difference = end_time - start_time
+        rate = n_iterations / time_difference
+
+        print("Processing rate: ", rate)
+
+        profile.print_stats()
+
+
+if __name__ == '__main__':
+    unittest.main()

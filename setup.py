f8a457a46a388df8b2240a37a3f5900f6e6ec87c
Andrej Babic <andrej.babic@psi.ch>
2018-09-24 14:03:35 +0200

    New library version
    - Get last processed image from client.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="psss_processing",
-      version="1.3.0",
+      version="1.4.0",
       author="Paul Scherrer Institute",
       description="PSSS image processing.",
       license="GPL3",

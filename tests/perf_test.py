c775ee1339bb974af76981f36bc3098e0d45c032
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2019-02-28 13:07:40 +0100

    use simulated 2d gaussian image
    using random numbers gives pessimistic performace in gaussian fitting.
    with simulated 2d gaussian image, the performace results is a bit
    over optimistic.

--- tests/perf_test.py
+++ tests/perf_test.py
@@ -4,7 +4,6 @@ from time import time
 import numpy
 import psss_processing.processor as processor
 
-import h5py
 
 class ImageProcessingPerformance(unittest.TestCase):
 
@@ -16,34 +15,39 @@ class ImageProcessingPerformance(unittest.TestCase):
         except ImportError:
             print("Please install the 'line_profiler' module first.")
             return
+        
+        # simulated image size
+        width = 2560
+        height = 2016
+        
+        # simulated gaussian function
+        xx, yy = numpy.meshgrid(numpy.arange(width), numpy.arange(height))
+        x0 = 1280 # x center
+        y0 = 1300 # y center
+        sx = 300  # x sigma
+        sy = 150  # y sigma
+        amplitude = 50
+        image = amplitude * numpy.exp(-(xx-x0)**2/(2*sx**2) - (yy-y0)**2/(2*sy**2))
+        noise = numpy.random.normal(scale=amplitude*0.2, size=(height, width))
 
-        #image = (numpy.random.rand(2016, 2560) * 200).astype(dtype="uint16")
-        #background_image = (numpy.random.rand(2016, 2560) * 10).astype(dtype="uint16")
-        f = h5py.File('/afs/psi.ch/user/w/wang_x1/background.h5')
-        background_image = f['/image'].value
-
-        images = []
-        for i in range(50):
-            images.append(h5py.File('/afs/psi.ch/user/w/wang_x1/%d.h5'%i)['/image'].value)
-
-        image = images[0]
+        image = (image + numpy.abs(noise)).astype(dtype="uint16")
+        background_image = (numpy.random.rand(2016, 2560) * 5).astype(dtype="uint16")
 
         roi = [900, 1600]
-        axis = numpy.linspace(9100, 9200, image.shape[1])
+        axis = numpy.linspace(8980, 9020, image.shape[1])
         parameters = {"background": "in_memory", "background_data": background_image}
 
         profile = LineProfiler(processor.process_image)
         process_image_wrapper = profile(processor.process_image)
 
         # Warm-up numba.
-        process_image_wrapper(image, axis, "image", roi, parameters)
+        results = processor.process_image(image, axis, "image", roi, parameters)
 
         n_iterations = 1000
 
         start_time = time()
 
         for i in range(n_iterations):
-            image = images[i%len(images)]
             process_image_wrapper(image, axis, "image", roi, parameters)
 
         end_time = time()

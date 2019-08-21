a993819408377670133d20c45d31b81ae7858555
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2019-08-21 08:53:27 +0200

    prepare 1.6.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="psss_processing",
-      version="1.6.0",
+      version="1.6.1",
       author="Paul Scherrer Institute",
       description="PSSS image processing.",
       license="GPL3",
